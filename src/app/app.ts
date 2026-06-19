0f9cdb7c6134f67f0d506b9f006fdbf59adc98bc
marioharo <haromario2@gmail.com>
2026-06-19 15:36:04 -0500

    feat: se agrega el diseño front (html y css) y la creacion del primer componente cursos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cursos } from './components/cursos/cursos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Cursos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
