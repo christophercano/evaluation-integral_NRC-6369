e00cdfb78ead7c7877da4d7dae0af631bfd36f2a
marioharo <haromario2@gmail.com>
2026-06-19 16:08:57 -0500

    feat: se añade el sistema de rutas mediante <RouterOutlet>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Cursos } from './components/cursos/cursos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'cursos', component: Cursos}
+];
