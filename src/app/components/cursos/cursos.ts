19889925def65200b196b9388d7ed38beb2c8eee
Alexis <alexis.cueva.chagua@gmail.com>
2026-07-19 19:32:08 -0500

    implementar registro de cursos con formulario reactivo

--- src/app/components/cursos/cursos.ts
+++ src/app/components/cursos/cursos.ts
@@ -1,20 +1,47 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { CursosModel, CursosService } from '../../services/cursos';
 import { RouterLink } from '@angular/router';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+
+import {
+  CursosModel,
+  CursosService
+} from '../../services/cursos';
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule
+  ],
   templateUrl: './cursos.html',
-  styleUrl: './cursos.css',
+  styleUrl: './cursos.css'
 })
 export class Cursos implements OnInit {
+
   cursos = signal<CursosModel[]>([]);
 
   private cursoService = inject(CursosService);
+  private fb = inject(FormBuilder);
+
+  cursoForm = this.fb.nonNullable.group({
+    curso: ['', Validators.required],
+    docente: ['', Validators.required],
+    categoria: ['', Validators.required],
+    inscritos: ['0', Validators.required],
+    precio: ['', Validators.required],
+    estado: ['Activo', Validators.required]
+  });
 
   ngOnInit(): void {
+    this.cargarCursos();
+  }
+
+  cargarCursos(): void {
     this.cursoService.getCurso().subscribe({
       next: (data) => {
         console.log('Cursos recibidos:', data);
@@ -23,6 +50,41 @@ export class Cursos implements OnInit {
       error: (error) => {
         console.error('Error al obtener cursos:', error);
       }
+    });
+  }
+
+  guardarCurso(): void {
+
+    if (this.cursoForm.invalid) {
+      this.cursoForm.markAllAsTouched();
+      return;
+    }
+
+    const nuevoCurso = this.cursoForm.getRawValue();
+
+    this.cursoService.crearCurso(nuevoCurso).subscribe({
+      next: () => {
+        alert('Curso registrado correctamente');
+
+        this.limpiarFormulario();
+
+        this.cargarCursos();
+      },
+      error: (error) => {
+        console.error('Error al registrar el curso:', error);
+        alert('No se pudo registrar el curso');
+      }
+    });
+  }
+
+  limpiarFormulario(): void {
+    this.cursoForm.reset({
+      curso: '',
+      docente: '',
+      categoria: '',
+      inscritos: '0',
+      precio: '',
+      estado: 'Activo'
     });
   }
 }
